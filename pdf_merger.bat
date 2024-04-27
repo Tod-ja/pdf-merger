ac0c3758ca4ad5b55f6a728e2b724150bb4708a8
Tod-ja <58312829+Tod-ja@users.noreply.github.com>
2024-04-27 16:08:35 +0200

    Add files via upload

--- pdf_merger.bat
+++ pdf_merger.bat
@@ -1,8 +1,13 @@
 @echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+:: Starting the Flask API
 echo Starting Flask API...
-start cmd /k "cd /d path\to\flask\app & set FLASK_APP=app.py & set FLASK_ENV=development & flask run"
+start cmd /k "python -m venv venv & call venv\Scripts\activate & set FLASK_APP=app.py & set FLASK_ENV=development & flask run"
 
+:: Starting the React App
 echo Starting React App...
-start cmd /k "cd /d path\to\react\app & npm start"
+start cmd /k "npm start"
 
+echo Both applications are starting...
 exit
