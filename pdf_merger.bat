8fe92632a4824e5047830b70a58fb5e44fd0085c
ChristopheJadot <christophe.jadot@sia-partners.com>
2024-05-19 14:41:27 +0200

    exe file

--- pdf_merger.bat
+++ pdf_merger.bat
@@ -6,11 +6,11 @@ cd /d %~dp0
 
 :: Starting the Flask API
 echo Starting Flask API...
-start cmd /k "call venv\Scripts\activate && set FLASK_APP=src\app.py && set FLASK_ENV=development && flask run"
+start /B "" cmd /C "call venv\Scripts\activate && set FLASK_APP=src\app.py && set FLASK_ENV=development && flask run"
 
 :: Starting the React App
 echo Starting React App...
-start cmd /k "npm start"
+start /B "" cmd /C "npm start"
 
-echo Both applications are starting...
+echo Both applications are starting in the background...
 exit
